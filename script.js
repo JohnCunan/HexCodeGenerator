c835ed1e7ed4f9695595197e6bf59c507d79e040
JohnCunan <76254034+JohnCunan@users.noreply.github.com>
2021-09-11 12:18:44 +0800

    Update

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let btnPrimary = document.querySelector('#primary');
+let btnCopy = document.querySelector('#copy')
 
 function changeColor() {
     var hex_numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
@@ -11,6 +12,7 @@ function changeColor() {
 
     document.getElementById("hex-code").innerHTML = hexcode;
     btnPrimary.style.backgroundColor = '#' + hexcode;
+    btnCopy.style.backgroundColor = '#' + hexcode;
     var color = '#' + hexcode;
 
     return color;
