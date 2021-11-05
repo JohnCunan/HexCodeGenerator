f092b7b2466b2aac8181ea3f17d0a1d8998ec129
JohnCunan <76254034+JohnCunan@users.noreply.github.com>
2021-11-05 21:25:03 +0800

    button added
    script for copy button not working yet

--- Script/script.js
+++ Script/script.js
@@ -1,4 +1,8 @@
 let btnChange = document.querySelector('#change');
+let btnCopy = document.querySelector('#copy');
+
+var color;
+
 
 function changeColor() {
     var hex_numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
@@ -11,10 +15,20 @@ function changeColor() {
 
     document.getElementById("hex-code").innerHTML = hexcode;
     btnChange.style.backgroundColor = '#' + hexcode;
-    var color = '#' + hexcode;
+    btnCopy.style.backgroundColor = '#' + hexcode;
+    color = '#' + hexcode;
+
+    
 
     return color;
 }
 
 btnChange.addEventListener('click', () =>
     document.body.style.backgroundColor = changeColor());
+
+btnCopy.addEventListener('click', () => 
+    alert("This feature will be added soon"));
+
+
+
+
