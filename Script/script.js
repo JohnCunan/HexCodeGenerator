b31694a325a4cdbd07a0bd569cbf1e38d811a63c
JohnCunan <76254034+JohnCunan@users.noreply.github.com>
2021-11-06 10:01:49 +0800

    Copy button update

--- Script/script.js
+++ Script/script.js
@@ -1,8 +1,6 @@
 let btnChange = document.querySelector('#change');
 let btnCopy = document.querySelector('#copy');
-
-var color;
-
+let color;
 
 function changeColor() {
     var hex_numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
@@ -13,22 +11,27 @@ function changeColor() {
         hexcode += hex_numbers[random_index];
     }
 
-    document.getElementById("hex-code").innerHTML = hexcode;
-    btnChange.style.backgroundColor = '#' + hexcode;
-    btnCopy.style.backgroundColor = '#' + hexcode;
     color = '#' + hexcode;
 
+    document.getElementById("hex-code").innerHTML = hexcode;
+    document.getElementById("box").value = color;
+    btnChange.style.backgroundColor = color;
+    btnCopy.style.backgroundColor = color;
     
-
     return color;
+}
+
+function getCode() {
+    var copyText = document.getElementById("box");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile version */
+
+    navigator.clipboard.writeText(copyText.value);
+    alert("Copied the hex code: " + copyText.value + " to the clipboard");
 }
 
 btnChange.addEventListener('click', () =>
     document.body.style.backgroundColor = changeColor());
 
 btnCopy.addEventListener('click', () => 
-    alert("This feature will be added soon"));
-
-
-
-
+    getCode());
